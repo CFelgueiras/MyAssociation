3d9fb86391cdc6556e15e322545bf1043090f3a6
CFelgueiras <48608692+CFelgueiras@users.noreply.github.com>
2020-02-20 12:04:36 +0000

    Update ConnectDB.java

--- src/main/java/myassociation/dal/ConnectDB.java
+++ src/main/java/myassociation/dal/ConnectDB.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 /**
- * Database connector class using Singleton Design Pattern
+ * Database connector class
  * @author Claudio Felgueiras
  */
 public class ConnectDB {
